784ce934d310e0993fbe3ca086bb2291f66e53d2
Henning Kayser <henningkayser@picknik.ai>
2022-09-22 12:15:46 +0000

    Add simple filter function

--- include/stomp_moveit/filter_functions.hpp
+++ include/stomp_moveit/filter_functions.hpp
@@ -3,10 +3,25 @@
 #include <Eigen/Geometry>
 #include <stomp_moveit/stomp_moveit_task.hpp>
 
+#include <stomp/utils.h>
+
 namespace stomp_moveit
 {
 namespace filters
 {
 const static FilterFn NoFilter = [](const Eigen::MatrixXd& values, Eigen::MatrixXd& filtered_values) { return true; };
+
+FilterFn simple_smoothing_matrix(size_t num_timesteps)
+{
+  Eigen::MatrixXd smoothing_matrix;
+  stomp::generateSmoothingMatrix(num_timesteps, 1.0, smoothing_matrix);
+  return [=](const Eigen::MatrixXd& values, Eigen::MatrixXd& filtered_values) {
+    for (size_t i = 0; i < filtered_values.rows(); ++i)
+    {
+      filtered_values.row(i).transpose() = smoothing_matrix * (filtered_values.row(i).transpose());
+    }
+    return true;
+  };
 }
+}  // namespace filters
 }  // namespace stomp_moveit
