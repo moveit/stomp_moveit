208de744db24186178ae8764c6320416f48c9559
Henning Kayser <henningkayser@picknik.ai>
2023-03-31 11:59:06 +0000

    Implement joint limit filter

--- include/stomp_moveit/filter_functions.hpp
+++ include/stomp_moveit/filter_functions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Geometry>
+#include <moveit/robot_model/joint_model_group.h>
 #include <stomp_moveit/stomp_moveit_task.hpp>
 
 #include <stomp/utils.h>
@@ -19,6 +20,33 @@ FilterFn simple_smoothing_matrix(size_t num_timesteps)
     for (int i = 0; i < filtered_values.rows(); ++i)
     {
       filtered_values.row(i).transpose() = smoothing_matrix * (filtered_values.row(i).transpose());
+    }
+    return true;
+  };
+}
+FilterFn enforce_position_bounds(const moveit::core::JointModelGroup* group)
+{
+  return [=](const Eigen::MatrixXd& values, Eigen::MatrixXd& filtered_values) {
+    filtered_values = values;
+    const auto& joints = group->getActiveJointModels();
+    for (size_t i = 0; i < joints.size(); ++i)
+    {
+      for (int j = 0; j < filtered_values.cols(); ++j)
+      {
+        joints.at(i)->enforcePositionBounds(&filtered_values.coeffRef(i, j));
+      }
+    }
+    return true;
+  };
+}
+FilterFn chain(const std::vector<FilterFn>& filter_functions)
+{
+  return [=](const Eigen::MatrixXd& values, Eigen::MatrixXd& filtered_values) {
+    Eigen::MatrixXd values_in = values;
+    for (const auto& filter_fn : filter_functions)
+    {
+      filter_fn(values_in, filtered_values);
+      values_in = filtered_values;
     }
     return true;
   };
